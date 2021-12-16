ec5a49f72d91b421e9f95229d00c34ad21d2e461
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-12-16 17:11:42 -0500

    verifications

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -48,7 +48,7 @@ const pizzaController = {
 
   // update pizza by id
   updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
     .then(dbPizzaData => {
       // if no pizza is found, send 404
       if (!dbPizzaData) {
